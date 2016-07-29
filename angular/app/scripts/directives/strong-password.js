8a1bd85789f9c34ce0aff8672c3db0ed38a9da5f
Łukasz Mazurek <kontakt@devoth.pl>
2016-07-29 18:59:54 +0200

    checking for strong password

---
+++ angular/app/scripts/directives/strong-password.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc directive
+ * @name angularApp.directive:strongPassword
+ * @description
+ * # strongPassword
+ */
+angular.module('angularApp')
+  .directive('strongPassword', function () {
+    return {
+      require: 'ngModel',
+      link: function(scope, element, attrs, ngModel) {
+
+        var hasUppercaseLetters = function(string) {
+          return string !== string.toLowerCase();
+        };
+
+        var hasLowercaseLetters = function(string) {
+          return string !== string.toUpperCase();
+        };
+
+        var isStrongPassword = function(pass) {
+          return pass.length >= 6 &&
+            /\d/.test(pass) &&
+            hasUppercaseLetters(pass) &&
+            hasLowercaseLetters(pass);
+        };
+
+        ngModel.$parsers.unshift(function(value) {
+           var valid = isStrongPassword(value);
+           ngModel.$setValidity('strongPassword', valid);
+           return value;
+        });
+
+      }
+    };
+  });
